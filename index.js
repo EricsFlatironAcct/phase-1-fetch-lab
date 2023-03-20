66f5f8d2f6773f3c72c5596bf28ae3739c7ba294
Eric Krukowski <erickruk@amazon.com>
2023-03-20 04:16:39 -0400

    Additional answers, 2nd fetch used since characters were saved as APIs rather than names

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let books = {};
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch("https://anapioficeandfire.com/api/books")
@@ -8,11 +7,21 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  console.log(`The 5th book in the series is ${books[4].name}`) //logs the 5th book in the series
+  //displays each book as a header and adds number of pages
+  let totalPages =0;
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    totalPages+=book.numberOfPages;
   });
+  console.log(`The total number of pages in the series is ${totalPages}`);
+  console.log(books);
+  //retrieves the 1031st character and logs to console
+  fetch("https://anapioficeandfire.com/api/characters/1031")
+  .then(resp =>resp.json())
+  .then(json=>console.log(`The 1031st character is ${json.name}`))
 }
 
 document.addEventListener('DOMContentLoaded', function() {
