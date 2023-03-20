0bad93b3dabc878e607a00f78e9db1c03cb622fe
Eric Krukowski <erickruk@amazon.com>
2023-03-20 03:58:34 -0400

    Lab passing both tests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+let books = {};
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp =>resp.json())
+  .then(json =>renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -13,5 +16,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+ fetchBooks();
+
 });
