1ec31d2b4e295a81ea45abe412ee76648b4f635f
Eric Krukowski <erickruk@amazon.com>
2023-03-20 04:19:13 -0400

    Reorganized code to log books at beginning instead of middle of logs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  console.log(`The 5th book in the series is ${books[4].name}`) //logs the 5th book in the series
+
   //displays each book as a header and adds number of pages
   let totalPages =0;
   books.forEach(book => {
@@ -16,12 +16,15 @@ function renderBooks(books) {
     main.appendChild(h2);
     totalPages+=book.numberOfPages;
   });
-  console.log(`The total number of pages in the series is ${totalPages}`);
   console.log(books);
+  console.log(`The 5th book in the series is ${books[4].name}`) //logs the 5th book in the series
+  console.log(`The total number of pages in the series is ${totalPages}`);
   //retrieves the 1031st character and logs to console
   fetch("https://anapioficeandfire.com/api/characters/1031")
   .then(resp =>resp.json())
   .then(json=>console.log(`The 1031st character is ${json.name}`))
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
